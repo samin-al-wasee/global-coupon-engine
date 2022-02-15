00ffecc274c2297952f667da49e7b2516b6cf65d
17r4f33d <sameenwasee@gmail.com>
2022-02-15 14:24:09 +0600

    Front-end update

--- promotion/models.py
+++ promotion/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Country(models.Model):
+	country_name = models.CharField(max_length=20, primary_key=True, verbose_name="Country Name")
+	country_flag = models.ImageField(upload_to="country_flags/", unique=True, verbose_name="Country Flag")
+	
+class Brand(models.Model):
+	brand_name = models.CharField(max_length=20, primary_key=True, verbose_name="Brand Name")
+	brand_logo = models.ImageField(upload_to="brand_logos/", unique=True, verbose_name="Brand Name")
