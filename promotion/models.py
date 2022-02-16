f994c8fce05b0c7c8b4bad8d430df911bf08b0c5
17r4f33d <sameenwasee@gmail.com>
2022-02-16 11:29:05 +0600

    Models and templates update

--- promotion/models.py
+++ promotion/models.py
@@ -1,27 +1,27 @@
-from django.db import models
 from datetime import datetime
-from uuid import uuid4
+from django.utils.text import slugify
+from django.db import models
 
 
 def rename_country_logos(instance, filename):
 	filebase, extension = filename.split('.')
 	if instance.pk:
-		return "images/country_logos/{}.{}".format(instance.pk, extension)
+		return "images/country_flags/{}.{}".format(slugify(instance.pk), extension)
 	else:
-		return "images/country_logos/{}.{}".format(instance.country_name, extension)
+		return "images/country_flags/{}.{}".format(slugify(instance.country_name), extension)
 
 
 def rename_brand_logo(instance, filename):
 	filebase, extension = filename.split('.')
 	if instance.pk:
-		return "images/brand_logos/{}.{}".format(instance.pk, extension)
+		return "images/brand_logos/{}.{}".format(slugify(instance.pk), extension)
 	else:
-		return "images/brand_logos/{}.{}".format(uuid4().hex(), extension)
+		return "images/brand_logos/{}.{}".format(slugify(instance.brand_name), extension)
 
 
 class Country(models.Model):
-	country_name = models.CharField(max_length=20, unique=True, verbose_name="Country Name")
-	country_logo = models.ImageField(upload_to=rename_country_logos, verbose_name="Country Logo")
+	country_name = models.CharField(max_length=20, unique=True, verbose_name="Name")
+	country_logo = models.FileField(upload_to=rename_country_logos, verbose_name="Logo")
 	
 	class Meta:
 		verbose_name = 'Country'
@@ -34,9 +34,9 @@ class Country(models.Model):
 
 class Brand(models.Model):
 	exists_in = models.ManyToManyField(Country, verbose_name='Countries')
-	brand_name = models.CharField(max_length=100, unique=True, verbose_name="Brand Name")
-	brand_logo = models.ImageField(upload_to=rename_brand_logo, verbose_name="Brand Logo")
-	brand_link = models.CharField(max_length=200, verbose_name="Brand URL")
+	brand_name = models.CharField(max_length=20, unique=True, verbose_name="Name")
+	brand_logo = models.FileField(upload_to=rename_brand_logo, verbose_name="Logo")
+	brand_link = models.CharField(max_length=200, verbose_name="URL")
 	
 	class Meta:
 		ordering = ['brand_name']
@@ -46,14 +46,19 @@ class Brand(models.Model):
 
 
 class Offer(models.Model):
-	offer_title = models.CharField(max_length=256, verbose_name="Offer Title")
+	choices_offer_type = [
+		("CODE", "Code"),
+		("DEAL", "Deal"),
+	]
+	offer_title = models.CharField(max_length=20, verbose_name="Title")
+	offer_details = models.TextField(max_length=200, verbose_name='Description')
 	offered_by = models.ForeignKey(Brand, on_delete=models.CASCADE, verbose_name='Brand')
-	offer_type = models.CharField(max_length=20, verbose_name='Offer Type')
 	offered_in = models.ForeignKey(Country, on_delete=models.CASCADE, verbose_name='Country')
-	offer_details = models.TextField(max_length=1000, verbose_name='Description')
+	offer_type = models.CharField(max_length=4, choices=choices_offer_type, verbose_name="Type")
 	offer_amount = models.FloatField(verbose_name='Amount')
-	offer_expires_on = models.DateTimeField(verbose_name='Expires On')
-	offer_next_available_on = models.DateTimeField(verbose_name='Next Available On')
+	offer_code = models.CharField(max_length=20, verbose_name="Code", help_text="Leave this field empty for a deal.", default="You don't need any code!")
+	offer_expires_on = models.DateField(verbose_name='Expires On')
+	offer_next_available_on = models.DateField(verbose_name='Next Available On')
 	offer_created_on = models.DateTimeField(auto_now_add=True, verbose_name='Created On')
 	offer_last_updated_on = models.DateTimeField(auto_now=True, verbose_name='Last Updated On')
 	
@@ -70,7 +75,7 @@ class Offer(models.Model):
 
 
 class Comment(models.Model):
-	username = models.CharField(max_length=256, verbose_name="Username")
+	username = models.CharField(max_length=50, verbose_name="Username")
 	comment = models.TextField(max_length=1000, verbose_name='Comment')
 	comment_created_on = models.DateTimeField(auto_now_add=True)
 	comment_last_updated_on = models.DateTimeField(auto_now=True)
