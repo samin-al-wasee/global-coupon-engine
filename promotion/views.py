52da5c59df4f899ef3526d3d8b260bcdddbd10ff
17r4f33d <sameenwasee@gmail.com>
2022-02-16 23:32:22 +0600

    Front-end update

--- promotion/views.py
+++ promotion/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from promotion.models import Country
+
 
 def homepage(request):
-	return render(request, "homepage.html")
+	countries = Country.objects.all()
+	context = {"countries": countries}
+	render(request, "country_list_footer.html", context)
+	return render(request, "homepage.html", context)
