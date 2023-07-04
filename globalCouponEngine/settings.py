e7088ac78a595e51a994e3100a4a23e31c50f9a9
Rafeed <sameenwasee@gmail.com>
2023-07-04 23:15:02 +0600

    vercel deploy with new config

--- globalCouponEngine/settings.py
+++ globalCouponEngine/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -112,7 +113,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "static"
+STATICFILES_DIRS = BASE_DIR / "static"
+STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
